96956a49e3cf85c07d8066ed2e50ca25c4541713
52toolsonline <52tools.info@gmail.com>
2026-08-27 00:18:47 +0530

    Update 52 Tools website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import mcp from 'astro-mcp';
-
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://52tools.online',
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [mcp(), sitemap()]
-});
+export default defineConfig({});
